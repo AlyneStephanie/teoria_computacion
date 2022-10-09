95b9d6a2ef2f408a7f9b010be88e9119ff22bcfb
AlyneStephanie <bigboss_snake@live.com.mx>
2022-10-09 03:02:35 -0500

    feat: refactored client and server

--- Primer_parcial/Pract2/client.py
+++ Primer_parcial/Pract2/client.py
@@ -1,38 +1,53 @@
 # Importamos socket al igual que con el servidor; sin embargo, su configuración será diferente
 import socket
 import random
+from common import *
+
+
+def make_list():
+
+    # Deben ser 64 caracteres
+    largo_cadena = 4
+    # Deben ser 1_000_000 de cadenas
+    num_cadenas = 10
+
+    for i in range(num_cadenas):
+       s = bin(random.randint(0, (2**largo_cadena) - 1))[2:].zfill(largo_cadena)
+       yield str(s)
 
 
 def main():
     # Generamos un nuevo objeto
-    my_socket = socket.socket()
+    client = socket.socket()
 
     # Hacemos la conexión
     # connect() va a recibir una tupla, y esta tupla va a contener dos valores: 1.- la dirección a la que nos tenemos que conectar, 2.- el puerto al que nos vamos a conectar. En el servidor establecimos el 8000
-    my_socket.connect(('localhost', 8000))
+    client.connect(('localhost', 8000))
 
-    # lista = 
 
     # Ahora enviaremos un mensaje
     for s in make_list():
-        my_socket.send(bytes('3' + s, encoding='utf-8'))
+        client.send(bytes(CODE_RECV_RM_STR + s, encoding=ENCODING))
         # Ahora vamos a recibir lo que el servidor nos responda. Para esto vamos a utilizar el método recv, y vamos a colocar 1024, lo cual hace referencia al bufer; es decir, 1024 bytes
-        response_serv = str(my_socket.recv(1024), encoding='utf-8')
+        response_serv = str(client.recv(BUFFER_SIZE), encoding=ENCODING)
+        print(response_serv)
 
-    my_socket.send(bytes('2', encoding='utf-8'))
+    client.send(bytes(CODE_RECV_RM_STR_END, encoding=ENCODING))
 
     while True:
-        resp = str(my_socket.recv(1024), encoding='utf-8')
+        resp = str(client.recv(BUFFER_SIZE), encoding=ENCODING)
         if len(resp) > 0:
             code = resp[0]
 
-            if code == '4':
+            if code == CODE_SEND_RESP_PROCESSED:
                 s = resp[1:]
-                my_socket.send(bytes("4", encoding='utf-8'))
+                print(resp)
+                client.send(bytes(CODE_SEND_RESP_PROCESSED, encoding=ENCODING))
 
-            if code == '5':
+            if code == CODE_SEND_RESP_PROCESSED_END:
+                print("Cliente Cerrando")
                 # Cerramos la conexión
-                my_socket.close()
+                client.close()
                 break
 
     
@@ -50,18 +65,6 @@ def main():
     # print(response_serv)
     
 
-
-
-def make_list():
-
-    # Deben ser 64 caracteres
-    largo_cadena = 4
-    # Deben ser 1_000_000 de cadenas
-    num_cadenas = 10
-
-    for i in range(num_cadenas):
-       s = bin(random.randint(0, (2**largo_cadena) - 1))[2:].zfill(largo_cadena)
-       yield str(s)
 
 
 
--- Primer_parcial/Pract2/server.py
+++ Primer_parcial/Pract2/server.py
@@ -1,74 +1,78 @@
 import socket
+from common import *
+
+
+def process_conn(conn):
+    odds = []
+    even = []
+    # Tenemos que enviar un montón de peticiones por cada conexión
+    while True:
+        # Podemos obtener lo que el cliente nos está enviando
+        request_str = str(conn.recv(BUFFER_SIZE), encoding=ENCODING)
+
+        if request_str[0] == CODE_RECV_RM_STR_END:
+            print(even)
+            print(odds)
+            code = None
+            for e in even:
+                conn.send(bytes(f"{CODE_SEND_RESP_PROCESSED}e{e}", encoding=ENCODING))
+                resp = str(conn.recv(BUFFER_SIZE), encoding=ENCODING)
+                code = resp[0]
+                if code == CODE_SEND_RESP_PROCESSED:
+                    continue
+                else:
+                    print(f"Error: código no esperado {code}")
+                    break
+
+            for o in odds:
+                conn.send(bytes(f"{CODE_SEND_RESP_PROCESSED}o{o}", encoding=ENCODING))
+                resp = str(conn.recv(BUFFER_SIZE), encoding=ENCODING)
+                code = resp[0]
+                if code == CODE_SEND_RESP_PROCESSED:
+                    continue
+                else:
+                    print(f"Error: código no esperado {code}")
+                    break
+
+            conn.send(bytes(CODE_SEND_RESP_PROCESSED_END, encoding=ENCODING))   
+
+            # Ahora cerramos la conexión con el cliente
+            conn.close()
+
+            break
+
+        if request_str[0] == CODE_RECV_RM_STR:
+            s = request_str[1:]
+            if is_even(s):
+                even.append(s)
+            else:
+                odds.append(s)
+
+        # Una vez con la petición, enviamos un mensaje al cliente:
+
+        conn.send(bytes(CODE_RECV_RM_STR, encoding=ENCODING))
+    
 
 def main():
     # Genera un socket con los valores por default
-    my_socket = socket.socket()
+    server = socket.socket()
 
     # Ahora establezco la conexión con el método bind, el cual recibe una tupla con dos valores: 1.- host, 2.- en qué puerto estará escuchando el socket
-    my_socket.bind(('localhost', 8000))
+    server.bind(('localhost', 8000))
 
     # Ahora establezco la cantidad de peticiones que puede manejar en cola nuestro socket
-    my_socket.listen(1)
+    server.listen(1)
 
     # Una vez establecida la conexión y que hayamos definido la cantidad de conexiones que puedan estar en cola, se establece un ciclo infinito
     while True:
         # Dentro de este ciclo, vamos a aceptar las peticiones de los clientes
         # Esta línea nos va a retornar dos valores: 1.- conexión, 2.- dirección
-        connection_cl, addr = my_socket.accept()
+        conn, addr = server.accept()
         print("Nueva conexión establecida")
         # Imprimimos la dirección de la cual se ha hecho la petición
         print(addr)
-        odds = []
-        even = []
-
-        # Tenemos que enviar un montón de peticiones por cada conexión
-        while True:
-            # Podemos obtener lo que el cliente nos está enviando
-            request_str = str(connection_cl.recv(1024), encoding='utf-8')
-
-            if request_str[0] == "2":
-                print(even)
-                print(odds)
-                
-                for e in even:
-                    connection_cl.send(bytes(f"4e{e}", encoding='utf-8'))
-                    resp = str(connection_cl.recv(1024), encoding='utf-8')
-                    if resp[0] == "4":
-                        continue
-                    else:
-                        print(f"Error: código no esperado {resp[0]}")
-                        break
-
-                for o in odds:
-                    connection_cl.send(bytes(f"4o{o}", encoding='utf-8'))
-                    resp = str(connection_cl.recv(1024), encoding='utf-8')
-                    if resp[0] == "4":
-                        continue
-                    else:
-                        print(f"Error: código no esperado {resp[0]}")
-                        break
-
-                connection_cl.send(bytes(f"5_end", encoding='utf-8'))   
-
-                # Ahora cerramos la conexión con el cliente
-                connection_cl.close()
-
-                break
-
-            if request_str[0] == "3":
-                s = request_str[1:]
-                if is_even(s):
-                    even.append(s)
-                else:
-                    odds.append(s)
-
-            # Una vez con la petición, enviamos un mensaje al cliente:
-
-            connection_cl.send(bytes("3", encoding='utf-8'))
-
-        
-
-    # Si corremos ahora el servidor, no ocurrirá nada, lo que significa que el servidor está a la espera. Ahora, lo que debemos hacer es generar el cliente.
+        process_conn(conn)
+ 
 
 def is_even(s):
     return  s.count('1') % 2 == 0
